63fb89f8163e3a8aaf86cb9720674c83a9c853ff
poundforpound <143540289+poundforpound@users.noreply.github.com>
2025-07-01 20:22:43 +0300

    finish lesson 2

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -10,15 +10,14 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType;
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[];
 };
 
 export const HW2 = () => {
-
   // 1️⃣ Раскомментируйте JSX (UserList2.tsx) и вы увидите,
   // что приложение начнет гореть красным и ругаться 😡
   // 2️⃣ Ваша задача: ❗ПОЧИНИТЬ ПРОЕКТ❗
@@ -30,7 +29,7 @@ export const HW2 = () => {
   // ❗ Массив с данными не трогаем!
 
   const users = {
-    myFriends:[
+    myFriends: [
       { id: 1, name: 'John', age: 25, address: { street: '123 Main St', city: 'New York' } },
       { id: 2, name: 'Alice', age: 30, address: { street: '456 Elm St', city: 'San Francisco' } },
       { id: 3, name: 'Bob', age: 35, address: { street: '789 Oak St', city: 'Seattle' } },
@@ -38,16 +37,31 @@ export const HW2 = () => {
       { id: 5, name: 'Mary', age: 13, address: { street: '456 Elm St', city: 'Los Angeles' } },
       { id: 6, name: 'James', age: 18, address: { street: '987 Pine Rd', city: 'Dallas' } },
       { id: 7, name: 'Jennifer', age: 21, address: { street: '654 Birch Ct', city: 'Seattle' } },
-      { id: 8, name: 'Robert', age: 4, address: { street: '876 Spruce Way', city: 'San Francisco' } },
-      { id: 9, name: 'Jessica', age: 12, address: { street: '543 Willow Dr', city: 'San Francisco' } },
-      { id: 10, name: 'Emily', age: 55, address: { street: '765 Aspen Blvd', city: 'Los Angeles' } },
-    ]
-  }
+      {
+        id: 8,
+        name: 'Robert',
+        age: 4,
+        address: { street: '876 Spruce Way', city: 'San Francisco' },
+      },
+      {
+        id: 9,
+        name: 'Jessica',
+        age: 12,
+        address: { street: '543 Willow Dr', city: 'San Francisco' },
+      },
+      {
+        id: 10,
+        name: 'Emily',
+        age: 55,
+        address: { street: '765 Aspen Blvd', city: 'Los Angeles' },
+      },
+    ],
+  };
 
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = users.myFriends.filter((user) => user.address.city === 'Los Angeles');
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
